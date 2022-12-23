448e439c73310c1a29e8a4ccc6ca2f75f0d3d6d6
lin <davidzhuprime@gmail.com>
2022-12-24 02:20:46 +0800

    second commit

--- main.cpp
+++ main.cpp
@@ -1,23 +1,74 @@
+// C++ program for the above approach
 #include <boost/thread.hpp>
 #include <boost/chrono.hpp>
 #include <iostream>
 
+#include <opencv2/core/core.hpp>
+
+// Library to include for
+// drawing shapes
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+using namespace cv;
+using namespace boost;
+
+
 void wait(int seconds)
 {
-    boost::this_thread::sleep_for(boost::chrono::seconds{seconds});
+  boost::this_thread::sleep_for(boost::chrono::seconds{seconds});
 }
 
-void thread()
+void thread_one()
 {
-    for (int i = 0; i < 5; ++i)
-    {
-        wait(1);
-        std::cout << i << '\n';
+  for (int i = 0; i < 5; ++i)
+  {
+    wait(1);
+    std::cout << i << '\n';
+  }
+}
+
+// Driver Code
+int main(int argc, char** argv)
+{
+
+      boost::thread t{thread_one};
+        t.join();
+    // Create a blank image of size
+    // (500 x 500) with black
+    // background (B, G, R) : (0, 0, 0)
+    Mat image(500, 500, CV_8UC3,
+              Scalar(0, 0, 0));
+
+    // Check if the image is created
+    // successfully
+    if (!image.data) {
+        std::cout << "Could not open or find"
+             << " the image";
+
+        return 0;
     }
-}
 
-int main()
-{
-    boost::thread t{thread};
-    t.join();
+    Point p1(0, 0), p2(100, 0);
+    Point p3(200, 0), p4(500, 500);
+    int thickness = 2;
+
+    // Line drawn using 8 connected
+    // Bresenham algorithm
+    line(image, p1, p4, Scalar(255, 0, 0),
+         thickness, LINE_8);
+
+    // Line drawn using 4 connected
+    // Bresenham algorithm
+    line(image, p2, p4, Scalar(0, 255, 0),
+         thickness, LINE_4);
+
+    // Antialiased line
+    line(image, p3, p4, Scalar(0, 0, 255),
+         thickness, LINE_AA);
+
+    // Show our image inside window
+    imshow("Output", image);
+    waitKey(0);
+
+    return 0;
 }
